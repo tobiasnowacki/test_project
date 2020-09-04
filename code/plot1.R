c9a9c15d2ec437147a5ac241ebada2aec6f17431
tobiasnowacki <tobynowacki@gmail.com>
2020-09-04 14:50:44 -0700

    lecture
    these are some edits from the lecture

--- code/plot1.R
+++ code/plot1.R
@@ -10,6 +10,7 @@ dat_agg <- import("modified_data/mt_agg.csv")[, -1]
 ggplot(dat_agg, aes(mean_hp, mean_mpg)) +
     geom_point(aes(colour = cyl)) +
     scale_colour_viridis(end = 0.8) +
-    theme_tn()
+    theme_tn() +
+    labs(x = "Mean Horsepower", y = "Mean MPG")
 ggsave("output/plot1.pdf", 
        height = 5, width = 6, device = cairo_pdf)
